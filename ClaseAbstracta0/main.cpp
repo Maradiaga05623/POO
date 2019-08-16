6539c7b6e4b4d5adb835c49af6869b6699b567c4
SergioPerezFigueroa <45744117+SergioPerezFigueroa@users.noreply.github.com>
2019-08-15 22:42:13 -0600

    Update main.cpp

--- ClaseAbstracta0/main.cpp
+++ ClaseAbstracta0/main.cpp
@@ -2,8 +2,24 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Hola mundo!" << endl;
-    return 0;
+int a,b,suma;
+
+cout<<"\n ingrese numero a:";
+
+cin>>a;
+
+cout<<"\n ingrese numero b:";
+
+cin>>b;
+
+suma=a+b;
+
+cout<<"\n la suma de "<<a<<"+"<<b<<" es:"<<suma;
+
+cout<<"hola mundo 2.0"
+
+return 0
+
+    
+    
 }
